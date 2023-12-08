324f586c978e0d6b5c05e18ab7bcb9bf558e37bd
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2023-12-08 11:36:52 +0200

    comm

--- src/components/Button/ButtonSvg.jsx
+++ src/components/Button/ButtonSvg.jsx
@@ -1,7 +1,10 @@
 import css from './ButtonSvg.module.css'
 
-export default function ButtonSvg ({styles,onClick,children,id})   {
-  
+export default function ButtonSvg ({styles,styles1,onClick,children,id})   {
+
+  // const style1 = styles ? css[styles] : '';
+  // const styles2 = styles1? css[styles] : '';
+
 return ( 
   <button type="button" className={css[styles]} id={id}  onClick={onClick}>{children}</button> 
 )
